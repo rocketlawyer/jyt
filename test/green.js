25be516d3f4058b374bb6e352b39e730e2b33a70
Christian Allen <christian@christianallen.com>
2013-10-05 19:52:49 -0400

    add tests for list and list shorthand

--- test/green.js
+++ test/green.js
@@ -26,4 +26,16 @@ describe("easy stuff", function() {
     it("html page with model and partial that extends a layout", function() {
         shouldCompileNodeFileTo("basicPageModelPartialExtends.jeff", {foo: "Goldie", bar: {baz: "Jeff", bam: "Christian"}}, "<!DOCTYPE html><html><head><title>I am a page title</title></head><body><div><h1>I am a header</h1><p>I am a section called Goldie</p><p class=\"2\"><div><p>this is a partial - holy crap!</p><a>and I can read from model too: Jeff</a></div></p></div><div><ol><li>option 1</li><li>option 2</li></ol></div></body></html>")
     })
+
+    it("list that uses the array method to shorthand lists", function() {
+        shouldCompileFileTo("list.jeff", {}, "<ol><li>this should be plain</li><li>and this too</li></ol>")
+    })
+
+    it("list at the top level", function() {
+        shouldCompileFileTo("listParent.jeff", {}, "<div>exposed</div><p>top level</p>")
+    })
+
+    it("list that uses the array method to shorthand lists", function() {
+        shouldCompileFileTo("listNested.jeff", {}, "<ol><li>this should be plain</li><li class=\"second\">but this should have a class</li><li><div>nested div</div><p>nested p</p></li><li><span>another way to nest</span></li></ol>")
+    })
 });
