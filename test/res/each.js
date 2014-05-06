f762f7414c7e8d93d1b3262c72327d44396bce4c
Christian Allen <christian@christianallen.com>
2014-05-06 15:47:13 -0400

    rearrange template tostring logic to accomodate rendering template in memory

--- test/res/each.js
+++ test/res/each.js
@@ -1,5 +1,5 @@
 module.exports = function(model) {
-    return ol(each(model.tweets, function(model) {
+    return ol(jeach(model.tweets, function(model) {
         return li([
             h1(model.title),
             p(model.content)
